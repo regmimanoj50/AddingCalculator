d7eaf5822fcab76add53bab2a6e52fbc21fd7104
Manoj Regmi <70725062+regmimanoj50@users.noreply.github.com>
2022-05-16 01:59:59 -0600

    Add files via upload

---
+++ Adding_calculator.py
@@ -0,0 +1,11 @@
+
+a = int(input ("Enter the first number: "))
+b = int(input ("Enter the second number: "))
+
+# adding first and second number to output final number
+sum = a + b
+
+#printing total
+print("Your total number is :" , sum)
+
+
